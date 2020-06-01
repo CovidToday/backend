dc6dc39712f3377d1f5a399e924b922b41c0db3f
siddharth jain <siddharthjain1611@gmail.com>
2020-06-01 22:44:03 +0530

    update state code

--- minimal/scripts/Rt_analysis.R
+++ minimal/scripts/Rt_analysis.R
@@ -2,7 +2,7 @@ library(EpiEstim)
 setwd(".")
 df = read.csv("dataset.csv")
 epid.count = as.numeric(unlist(c(df['active'])))
-window = 5
+window = 7
 res.R = estimate_R(epid.count,method = "uncertain_si",config = make_config(list(t_start=2:(length(epid.count)-window),t_end=(2+window):length(epid.count),mean_si = 3.96, std_mean_si = 0.215,
                                                                                  min_mean_si = 3.53, max_mean_si = 4.39,
                                                                                  std_si = 4.75, std_std_si = 0.145,
