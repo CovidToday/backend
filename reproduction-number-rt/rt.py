754f6ffdeb1f4ba9cfa90b7fb26f0ed973962643
Pratik Mandlecha <pmandlecha198@gmail.com>
2020-06-11 18:15:19 +0530

    rt.py updated as per new ipynb and CSV added

--- reproduction-number-rt/rt.py
+++ reproduction-number-rt/rt.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 get_ipython().system('pip install wget')
 
 
-# In[1]:
+# In[ ]:
 
 
 import numpy as np
@@ -19,37 +19,37 @@ import json
 import wget
 import os
 import os.path
+from google.colab import drive
+drive.mount('/content/gdrive')
+os.chdir('/content/gdrive/My Drive/minimal')
+
+
+# In[ ]:
+
 
 if os.path.exists(os.getcwd()+"\\national.json"):
     os.remove(os.getcwd()+"\\national.json")
-wget.download('https://api.covid19india.org/data.json', os.getcwd()+"\\national.json")
+wget.download('https://api.covid19india.org/data.json', os.getcwd()+"//national.json")
 
 if os.path.exists(os.getcwd()+"\\states.json"):
     os.remove(os.getcwd()+"\\states.json")
-wget.download('https://api.covid19india.org/states_daily.json', os.getcwd()+"\\states.json")
+wget.download('https://api.covid19india.org/states_daily.json', os.getcwd()+"//states.json")
 
 
-# In[2]:
-
-
-import sys
-sys.path
-
-
-# In[3]:
+# In[ ]:
 
 
 def pooled_SD(sds,means):
     return np.sqrt((np.sum(sds**2,axis=0)+np.sum(means-np.mean(means,axis=0)))/sds.shape[0])
 
 
-# In[4]:
+# In[ ]:
 
 
 json_data = {}
 
 
-# In[5]:
+# In[ ]:
 
 
 def fn(mon):
@@ -83,8 +83,7 @@ def convert(dat):
 
 # ### Calculation for Rt (India - No Import Adjustment)
 
-# In[6]:
-
+# In[ ]:
 
 
 dates = np.array([pd.to_datetime(i['date']+"2020") for i in json.load(open('national.json',))['cases_time_series']])
@@ -97,7 +96,7 @@ real_data = confirmed
 
 rt = []
 dats = []
-for n in range(10):
+for n in range(100):
     print("Iteration: ",n+1,end='\r')
     dataset = np.copy(real_data)
     G = gamma(3.325+0.616*np.random.normal(),0.979+0.195*np.random.normal())
@@ -120,7 +119,7 @@ means = np.array([x["Mean(R)"].values for x in rt])
 sds = np.array([x["Std(R)"].values for x in rt])
 
 
-# In[7]:
+# In[ ]:
 
 
 stindex = 2+5-1
@@ -130,10 +129,21 @@ for i in unchanged:
   changed.append(convert(i))
 
 
-# In[8]:
+# In[ ]:
 
 
-
+import pandas as pd
+df=pd.DataFrame()
+csv_dates=[]
+csv_rt_point=[]
+csv_rt_sd=[]
+csv_rt_l95=[]
+csv_rt_u95=[]
+csv_rt_l50=[]
+csv_rt_u50=[]
+csv_cases_mean=[]
+csv_cases_sd=[]
+csv_cases_dates=[]
 #dates=[]
 india = {
         'dates':changed,
@@ -147,9 +157,31 @@ india = {
         'cases_sd':list(np.std(dats,axis=0)),
         'cases_dates':list(pd.Series(dates)[:-3].dt.strftime('%m-%d-%Y'))
         }
+for i in range(len(changed)):
+  csv_dates.append(changed[i])
+  csv_rt_point.append(list(means.mean(axis=0))[i])
+  csv_rt_sd.append(list(pooled_SD(sds,means))[i])
+  csv_rt_l95.append(list(means.mean(axis=0)-1.95996*pooled_SD(sds,means))[i])
+  csv_rt_u95.append(list(means.mean(axis=0)+1.95996*pooled_SD(sds,means))[i])
+  csv_rt_l50.append(list(means.mean(axis=0)-0.67449*pooled_SD(sds,means))[i])
+  csv_rt_u50.append(list(means.mean(axis=0)+0.67449*pooled_SD(sds,means))[i])
+  csv_cases_mean.append(list(np.mean(dats,axis=0))[i])
+  csv_cases_sd.append(list(np.std(dats,axis=0))[i])
+  csv_cases_dates.append(list(pd.Series(dates)[:-3].dt.strftime('%m-%d-%Y'))[i])
+df['dates']=csv_dates
+df['state']=['India']*len(csv_dates)
+df['rt_point']=csv_rt_point
+df['rt_sd']=csv_rt_sd
+df['rt_l95']=csv_rt_l95
+df['rt_u95']=csv_rt_u95
+df['rt_l50']= csv_rt_l50
+df['rt_u50']=csv_rt_u50
+df['cases_mean']=csv_cases_mean
+df['cases_sd']=csv_cases_sd
+df['cases_dates']=csv_cases_dates
 
 
-# In[9]:
+# In[ ]:
 
 
 json_data['IN'] = india
@@ -157,13 +189,13 @@ json_data['IN'] = india
 
 # ### State Level Data
 
-# In[10]:
+# In[ ]:
 
 
 states = list(filter(lambda v:len(v)<3,list(json.load(open('states.json',))['states_daily'][0].keys())))
 
 
-# In[11]:
+# In[ ]:
 
 
 dates = np.array([pd.to_datetime(i['date']) for i in filter(lambda v: v['status'] == 'Confirmed',json.load(open('states.json',))['states_daily'])])
@@ -172,7 +204,7 @@ for st in states:
     data[st] = np.array([i[st] for i in filter(lambda v: v['status'] == 'Confirmed',json.load(open('states.json',))['states_daily'])])
 
 
-# In[12]:
+# In[ ]:
 
 
 data = data.replace(r'^\s*$', np.NaN, regex=True).fillna(0)
@@ -180,7 +212,7 @@ data = data.astype(np.int32)
 data['date'] = dates
 
 
-# In[13]:
+# In[ ]:
 
 
 state_id = {
@@ -220,14 +252,23 @@ state_id = {
   "dn":"Dadra and Nagar Haveli and Daman and Diu",
   "sk":"Sikkim",
 }
-
 for state in state_id.keys():
+    csv_dates=[]
+    csv_rt_point=[]
+    csv_rt_sd=[]
+    csv_rt_l95=[]
+    csv_rt_u95=[]
+    csv_rt_l50=[]
+    csv_rt_u50=[]
+    csv_cases_mean=[]
+    csv_cases_sd=[]
+    csv_cases_dates=[]
     print("\n",state)
-    boots = 10
+    boots = 100
     real_data = data[state].values
-    df = pd.DataFrame()
-    df['active'] = real_data
-    df.to_csv('dataset.csv',index=False)
+    active = pd.DataFrame()
+    active['active'] = real_data
+    active.to_csv('dataset.csv',index=False)
     rt = []
     dats = []
     for n in range(boots):
@@ -241,11 +282,11 @@ for state in state_id.keys():
             for j in np.unique(np.int32(send_back)):
                 dataset[i-j] += np.sum(send_back==j)
         dataset[::-1] = dataset[::-1]+np.random.negative_binomial(n=dataset[::-1]+1,p=G.cdf(np.arange(len(dataset))),size=len(dataset)) 
-        df = pd.DataFrame()
-        df['active'] = dataset[:-3]
-        df['date'] = dates[:-3]
+        active = pd.DataFrame()
+        active['active'] = dataset[:-3]
+        active['date'] = dates[:-3]
         dats.append(dataset[:-3])
-        df.to_csv('dataset.csv',index=False)
+        active.to_csv('dataset.csv',index=False)
         call(['RScript.exe','scripts/Rt_analysis.R'])
         rt.append(pd.read_csv('rtoutput.csv'))
 
@@ -271,13 +312,43 @@ for state in state_id.keys():
             'cases_sd':list(np.std(dats,axis=0)),
             'cases_dates':list(pd.Series(dates)[:-3].dt.strftime('%m-%d-%Y'))
             }
+    state_df=pd.DataFrame()
+    for i in range(len(changed)):
+      csv_dates.append(changed[i])
+      csv_rt_point.append(list(means.mean(axis=0))[i])
+      csv_rt_sd.append(list(pooled_SD(sds,means))[i])
+      csv_rt_l95.append(list(means.mean(axis=0)-1.95996*pooled_SD(sds,means))[i])
+      csv_rt_u95.append(list(means.mean(axis=0)+1.95996*pooled_SD(sds,means))[i])
+      csv_rt_l50.append(list(means.mean(axis=0)-0.67449*pooled_SD(sds,means))[i])
+      csv_rt_u50.append(list(means.mean(axis=0)+0.67449*pooled_SD(sds,means))[i])
+      csv_cases_mean.append(list(np.mean(dats,axis=0))[i])
+      csv_cases_sd.append(list(np.std(dats,axis=0))[i])
+      csv_cases_dates.append(list(pd.Series(dates)[:-3].dt.strftime('%m-%d-%Y'))[i])
+    state_df['dates']=csv_dates
+    state_df['state']=[state]*len(csv_dates)
+    state_df['rt_point']=csv_rt_point
+    state_df['rt_sd']=csv_rt_sd
+    state_df['rt_l95']=csv_rt_l95
+    state_df['rt_u95']=csv_rt_u95
+    state_df['rt_l50']= csv_rt_l50
+    state_df['rt_u50']=csv_rt_u50
+    state_df['cases_mean']=csv_cases_mean
+    state_df['cases_sd']=csv_cases_sd
+    state_df['cases_dates']=csv_cases_dates
+    df=pd.concat([df,state_df])
     json_data[state] = temp
 
 
-# In[14]:
+# In[ ]:
 
 
-with open('data.json', 'w') as outfile:
+df.to_csv('rt.csv',index=False)
+
+
+# In[ ]:
+
+
+with open('rt.json', 'w') as outfile:
     json.dump(json_data, outfile)
 
 
