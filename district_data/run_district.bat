776744db3b420a9b17a1ce4da7b232d1fc21f6ce
Mohak Gupta <xmohakgupta@gmail.com>
2020-09-02 05:55:29 +0530

    Updated on 02-Sep-20

--- district_data/run_district.bat
+++ district_data/run_district.bat
@@ -1,4 +1,9 @@
 call F:\Anaconda3\Scripts\activate.bat
 python unified_districts_metrics.py
-pause
-
+git pull 
+git status
+git add --all
+git commit -m "Updated on %date%"
+git push --all
+echo TODAY'S UPDATE IS COMPLETE 
+shutdown /s
--- state_data/run_state.bat
+++ state_data/run_state.bat
@@ -1,8 +1,4 @@
 call F:\Anaconda3\Scripts\activate.bat
 python unified_states_metrics.py
-git pull https://aberrantdoc:Mgupta3110G@github.com/CovidToday/backend
-git add --all
-git commit -m "Update"
-git push https://aberrantdoc:Mgupta3110G@github.com/CovidToday/backend --all
 pause
 
