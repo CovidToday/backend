5ad484069a0497e36aa5e126521706e66cccf02f
Mohak Gupta <xmohakgupta@gmail.com>
2020-09-03 14:10:34 +0530

    Updated on 03-Sep-20

--- district_data/run_district.bat
+++ district_data/run_district.bat
@@ -1,9 +1,8 @@
 call F:\Anaconda3\Scripts\activate.bat
-#python unified_districts_metrics.py
+python unified_districts_metrics.py
 git pull 
 git status
 git add --all
 git commit -m "Updated on %date%"
 git push --all
 echo TODAY'S UPDATE IS COMPLETE 
-shutdown /s
