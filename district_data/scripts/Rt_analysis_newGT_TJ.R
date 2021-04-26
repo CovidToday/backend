ca5c78931a9df5e6ba33c5d56e1636b521465be7
Mohak Gupta <63073968+aberrantdoc@users.noreply.github.com>
2021-04-27 02:25:56 +0530

    Update Rt script with new Serial Interval value
    SI value according to Gupta et al https://www.medrxiv.org/content/10.1101/2020.12.25.20248668v1

--- district_data/scripts/Rt_analysis_newGT_TJ.R
+++ district_data/scripts/Rt_analysis_newGT_TJ.R
@@ -4,9 +4,9 @@ df = read.csv("confirmed.csv")
 epid.count = as.numeric(unlist(c(df['active'])))
 window = 7
 res.R = estimate_R(epid.count,method = "uncertain_si",config = make_config(list(t_start=2:(length(epid.count)-window),t_end=(2+window):length(epid.count),
-                                                                                mean_si = 3.95, std_mean_si = 0.475,
-                                                                                min_mean_si = 3.01, max_mean_si = 4.91,
-                                                                                std_si = 1.51, std_std_si = 0.5575,
-                                                                                min_std_si = 0.74, max_std_si = 2.97,
+                                                                                mean_si = 5.53, std_mean_si = 0.487,
+                                                                                min_mean_si = 4.58, max_mean_si = 6.53,
+                                                                                std_si = 3.47, std_std_si = 0.417,
+                                                                                min_std_si = 2.59, max_std_si = 4.26,
                                                                                 n1 = 114, n2 = 114,mean_prior=2.6,std_prior=2)))
 write.csv(res.R$R,'rt_temp/rtoutput.csv',row.names = TRUE)
