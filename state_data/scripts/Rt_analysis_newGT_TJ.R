e1302af910d500666503ac39f5d7cdbbd5e7afff
Mohak Gupta <63073968+aberrantdoc@users.noreply.github.com>
2021-04-27 17:16:07 +0530

    Update Rt_analysis_newGT_TJ.R
    Revert to window=7

--- state_data/scripts/Rt_analysis_newGT_TJ.R
+++ state_data/scripts/Rt_analysis_newGT_TJ.R
@@ -2,7 +2,7 @@ library(EpiEstim)
 setwd(".")
 df = read.csv("confirmed.csv")
 epid.count = as.numeric(unlist(c(df['active'])))
-window = 10
+window = 7
 res.R = estimate_R(epid.count,method = "uncertain_si",config = make_config(list(t_start=2:(length(epid.count)-window),t_end=(2+window):length(epid.count),
                                                                                 mean_si = 5.53, std_mean_si = 0.487,
                                                                                 min_mean_si = 4.58, max_mean_si = 6.53,
